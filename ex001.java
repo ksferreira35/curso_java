99104f3a06f0f62a91cc5e1b0d7099ed4b3aa88c
Kaiky Ferreira <contatokaiky.sf21@gmail.com>
2025-02-13 16:29:04 -0300

    Ex's Done!

--- ex001.java
+++ ex001.java
@@ -1,6 +1,5 @@
 // Programa que escreve Hello World!
 public class ex001 {
-
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
---
+++ ex002.java
@@ -0,0 +1,14 @@
+// Programa que mostra a data e hora do sistema.
+import java.util.Date;
+
+public class ex002 {
+    public static void main(String[] args) {
+        Date clock = new Date();
+
+        System.out.println("A hora do sistema é");
+        
+        System.out.println(clock.toString());
+
+    }
+}
+// Desenvolvido por Kaiky - 2025
