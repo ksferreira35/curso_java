2ccef63734f429288aee01990df18693f5758f10
Kaiky Ferreira <contatokaiky.sf21@gmail.com>
2025-02-13 16:29:04 -0300

    Ex's Done!

---
+++ ex003.java
@@ -0,0 +1,12 @@
+// Programa que escreve o idioma do sistema.
+import java.util.Locale;
+
+public class ex003 {
+    public static void main(String[] args) {
+
+    Locale sistema = Locale.getDefault();
+
+    System.out.println("A linguagem do sistema é: " + sistema.getDisplayLanguage());   
+    }
+}
+// Desenvolvido por Kaiky - 2025
---
+++ ex004.java
@@ -0,0 +1,13 @@
+// Programa que mostra a resolução do sistema.
+import java.awt.Toolkit;
+
+public class ex004 {
+    public static void main(String[] args) {
+        
+        Toolkit tela = Toolkit.getDefaultToolkit();
+
+        System.out.println("A resolução do sistema é: " + tela.getScreenSize().width + "x" + tela.getScreenSize().height);
+        
+    }
+}
+// Desenvolvido por Kaiky - 2025
