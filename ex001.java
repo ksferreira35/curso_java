94eee0ae1621fd747cfdbbcee7dbce7ae140993b
Kaiky Ferreira <contatokaiky.sf21@gmail.com>
2025-02-13 15:03:40 -0300

    initial commit

---
+++ ex001.java
@@ -0,0 +1,10 @@
+// Programa que escreve Hello World!
+public class ex001 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, World!");
+
+    } 
+}
+// Desenvolvido por Kaiky - 2025
