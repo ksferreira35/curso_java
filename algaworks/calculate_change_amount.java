c1ab0c0afd7422c6a92d9dd11243949823f5ff57
Kaiky Ferreira <contatokaiky.sf21@gmail.com>
2025-04-01 15:45:20 -0300

    New Java course!

---
+++ algaworks/calculate_change_amount.java
@@ -0,0 +1,24 @@
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+public class calculate_change_amount {
+    @SuppressWarnings("ConvertToTryWithResources") // The scanner is underlined, so I added this to prevent it from being underlined in VSCode.
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("What is the price of the product? ");
+        Double productPrice = scanner.nextDouble();
+
+        System.out.print("Quantity provided by the customer? ");
+        Double priceProvided = scanner.nextDouble();
+
+        Double result = priceProvided - productPrice;
+
+        DecimalFormat df = new DecimalFormat("0.00");
+        System.out.println("The customer's change is R$" + df.format(result));
+
+        scanner.close();
+    }
+
+}
+
