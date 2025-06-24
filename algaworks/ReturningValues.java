e8121b1f7c5c3d74dc11e4cd2681ed909a4ab378
Kaiky Ferreira <contatokaiky.sf21@gmail.com>
2025-06-24 01:33:16 -0300

    Some Ex's Done!

---
+++ algaworks/ReturningValues.java
@@ -0,0 +1,16 @@
+public class ReturningValues {
+    public static void main(String[] args) {
+        Double[] premissaInicial = new Double[] { 50.0, 100.0 };
+
+        Double proporcao = 20.0;
+        Double resultadoFinal = regraDeTres(premissaInicial, proporcao);
+
+        System.out.println("O resultado é: " + resultadoFinal);
+    }
+
+    static Double regraDeTres(Double[] premissa, Double proporcacao) {
+        Double resultado = (premissa[0] * proporcacao) / premissa[1];
+        return resultado;
+    }
+}
+    
